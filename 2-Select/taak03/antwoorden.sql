251dadcc6cba68bffbe1fd61ed79f055a19dd524
Tarik Ünal <2108287@talnet.nl>
2020-06-04 12:07:34 +0200

    taak 1, deel 2 taak 1 2 en een beetje van 3

--- 2-Select/taak03/antwoorden.sql
+++ 2-Select/taak03/antwoorden.sql
@@ -1,13 +1,13 @@
 -- Opdracht 1 
-
+SELECT * FROM `players` WHERE nationality = "Spain" AND club = "Chelsea"
 -- Opdracht 2 
-
+SELECT * FROM `players` WHERE age = 17 AND nationality = "Spain" AND wage = 15000
 -- Opdracht 3
-
+SELECT * FROM `players` WHERE `club` = "liverpool" AND age > 20
 -- Opdracht 4
-
+SELECT * FROM `players` WHERE nationality = "Netherlands" AND club = "Ajax"
 -- Opdracht 5
-
+SELECT * FROM `players` WHERE club = "Ajax" AND nationality != "Netherlands"
 -- Opdracht 6 
 
 -- Opdracht 7 
